d62dcd7c004fde6a5c5e31823d7d4078a6b79dde
k-hadidja <hadidja.s@gmail.com>
2020-01-28 17:03:58 +0100

    Modif méthode pour création d'un map a partir d'une map existante
    calcul de la moyenne, la methode renvoie un nouvelle map avec des valeurs obtenue par calcul

--- src/main/java/fr/gustaveroussy/AdvancedQC/AdvancedQcApplication.java
+++ src/main/java/fr/gustaveroussy/AdvancedQC/AdvancedQcApplication.java
@@ -5,6 +5,7 @@ package fr.gustaveroussy.AdvancedQC;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -34,6 +35,8 @@ public class AdvancedQcApplication implements CommandLineRunner {
 		
 	}
 
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
@@ -41,7 +44,8 @@ public class AdvancedQcApplication implements CommandLineRunner {
 			List<String> lines = Files.readAllLines(Paths.get(args[0]), StandardCharsets.UTF_8);  
 	
 		
-			renvoieDonneesTraitees(lines);
+			renvoieDonneesTraitees(lines);//données exploitables création de la premiere map
+			remplirMapPourcent(toutesMesMaps);
 		}else {
 			LOG.error("args must be 1");
 		}	
@@ -49,33 +53,10 @@ public class AdvancedQcApplication implements CommandLineRunner {
 	}
 	
 	
-//public Set<Map<String,Double>> renvoieDonneesTraitees (List<String> lignesdefichier){
-//		 // TODO Auto-generated method stub
-//	Set<Map<String, Double>> toutesMesMaps = new HashSet<Map<String, Double>>();
-//	
-//	Map<String,Double> maMap = null;
-//	lignesdefichier.remove(0); //elimination du header de la liste "lines"
-//		 String[] toutesmescolonnes = lignesdefichier.get(0).split("\t");	
-//			for (int i = 1; i<toutesmescolonnes.length; i++) {
-//				maMap=transformListenTabExploitable(lignesdefichier, i);	
-//				pourcentageTotal (maMap);
-//				LOG.info("Map de l'echantillon "+ i + ": "+maMap.toString());
-//			    remplirMapPourcent(maMap);
-//			    toutesMesMaps.add(maMap);
-//			    
-//			}
-//		  LOG.info ( "Mes Maps: " + toutesMesMaps);//renvoie les maps avec les valeurs d'origine et non les valeurs calculées
-//	
-//			return (toutesMesMaps);
-//	}
-	
-
-//****************Test modif: RENVOIE DONNEES TRAITEES, pour obtenir un set de données qui sera réutiliser par les autre parties du prgrm
-	
+//**************** Modif méthode RENVOIE DONNEES TRAITEES, pour obtenir un set de données qui sera réutiliser par les autre parties du prgrm
+	Set<Map<String, Double>> toutesMesMaps = new HashSet<Map<String, Double>>();
 public Set<Map<String,Double>> renvoieDonneesTraitees (List<String> lignesdefichier){
 	 // TODO Auto-generated method stub
-Set<Map<String, Double>> toutesMesMaps = new HashSet<Map<String, Double>>();
-
 Map<String,Double> maMap = null;
 lignesdefichier.remove(0); //elimination du header de la liste "lines"
 	 String[] toutesmescolonnes = lignesdefichier.get(0).split("\t");	
@@ -90,13 +71,12 @@ lignesdefichier.remove(0); //elimination du header de la liste "lines"
 
 	
 //Méthode pour créer, à partir du fichier .tsv, un tableau de données exploitable
-	
+Map<String,Double> maMap = new HashMap<String,Double>();
 public Map<String, Double> transformListenTabExploitable(List<String> lignesatransformer, Integer numechantillon) {
 		// TODO Auto-generated method stub
 	Map<String,Double> maMap = new HashMap<String,Double>();
 		for(String str : lignesatransformer){	
 			 String[] toutesmescolonnes = str.split("\t");			
-//				LOG.info("Mes lignes [0]" +toutesmeslignes[0]);// renvoie bien le nom de tout les genes
 				String valeurStringdufichier = toutesmescolonnes [numechantillon]; 
 				double valeurDoubledufichier = Double.parseDouble (valeurStringdufichier);	
 				maMap.put(toutesmescolonnes [0], valeurDoubledufichier);
@@ -104,8 +84,6 @@ public Map<String, Double> transformListenTabExploitable(List<String> lignesatra
 		
 		return (maMap) ;
 		}
-
-
 
 
 //Methode pour compter les valeurs nulles par échantillon
@@ -122,7 +100,6 @@ public Map<String, Double> transformListenTabExploitable(List<String> lignesatra
 		return Counter;
 	}
 	
-
 	
 //Methode pour calculer le Pourcentage
 	
@@ -134,28 +111,76 @@ public Map<String, Double> transformListenTabExploitable(List<String> lignesatra
 	LOG.info("le pourcentage de valeurs nulles dans cet échantillon vaut: "+ pourcentageTotal + " %");
 	return pourcentageTotal;
 	}
-	
-	
-	
-	
-//Methode pour créer un nouvelle map à partir d'une map déjà existante	
-	
-	public Map<String, Double> remplirMapPourcent(Set<Map<String, Double>> monSetDeMaps) {
-		// TODO Auto-generated method stub	 		 
-		 for(Entry<Set<Map<String, Double>>> monSet: monSetDeMaps) {
-			   valeurmaMap2Final = (somme des valeurs pour une key *100)/toutesMesMaps.size());
-			   maMap2.put(keymaMap2, valeurmaMap2Final);
-		 }
-		 
-		// LOG.info(" Voici la nouvelle hashmap: "+maMap2.toString());
-		 //LOG.info(" Ma nouvelle : "+maMap2.toString());
-		 LOG.info("taille de ma collection de map "+toutesMesMaps.size());
-			return (maMap2);
 		
-			
-	}
+	
+//Modif méthode pour CREATION DU NVL MAP À PARTIR DE MAP DEJA EXISTANTE, la meth doit prendre en entrée un set de maps et renvoyer une map ac % taux expression	
+	
+	public Map<String, Double> remplirMapPourcent(Set<Map<String, Double>> toutesMesMaps) {
+		System.out.println(toutesMesMaps);
+		// TODO Auto-generated method stub	 	
+		Map<String, Double> duplicatMap = new HashMap<>();
+		Map<String,Double> mouenne = new HashMap<>();
+		Iterator it = toutesMesMaps.iterator();
+      
 		
-		 
-	
-	
+		int i = 0;
+       
+        while(it.hasNext()) {
+        	duplicatMap = (Map<String, Double>) it.next();
+            
+            
+            Set clefs = duplicatMap.keySet();
+            
+            Iterator itbis = clefs.iterator();
+            i = i + 1;
+            
+            
+            while(itbis.hasNext()){
+            	
+            	String clef = itbis.next().toString();
+            	
+            	if(!mouenne.containsKey(clef)) {
+            	
+            		mouenne.put(clef, duplicatMap.get(clef));
+            	
+            	} else {
+            		
+            		Double valeur = mouenne.get(clef);
+            		mouenne.put(clef, duplicatMap.get(clef) + valeur);
+            		
+            	}
+            	
+            }
+            
+            
+            
+            System.out.println("test");
+        }
+        
+        System.out.println(mouenne);
+        
+        Set clefs = mouenne.keySet();
+        Iterator itbis = clefs.iterator();
+        
+        Map<String,Double> mouennebis = new HashMap<>();
+        System.out.println(i);
+        while(itbis.hasNext()){
+        	String clef = itbis.next().toString();
+        	Double valeur = mouenne.get(clef)/i;
+//        	System.out.println(valeur);
+        	mouennebis.put(clef, valeur);
+//        	System.out.println(mouenne);
+        	System.out.println(clef);
+
+        	
+        }
+//        System.out.println(i);
+        System.out.println(mouennebis);
+        return (mouennebis);					
 }
+		
+}	
+		 
+	
+	
+

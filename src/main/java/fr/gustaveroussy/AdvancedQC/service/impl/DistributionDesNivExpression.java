527b9cb57be2d8330cb474f3ff875d873b25933c
k-hadidja <hadidja.s@gmail.com>
2020-03-17 18:32:00 +0100

    modification methode Distribnivexpression en cours
    la methode prend desormais en argument Samplwheader pour faciliter l'utilisation du résultat dans json

--- src/main/java/fr/gustaveroussy/AdvancedQC/service/impl/DistributionDesNivExpression.java
+++ src/main/java/fr/gustaveroussy/AdvancedQC/service/impl/DistributionDesNivExpression.java
@@ -16,57 +16,81 @@ import fr.gustaveroussy.AdvancedQC.service.IDistributionNivExpression;
 public class DistributionDesNivExpression implements IDistributionNivExpression {
 
 //DECILES
-	@Override
-	public List<SampleValue> calculDecileMin(List<SamplewHeader> listwHeader) {
+	public List<SampleValue> calculDecileMin(SamplewHeader sample) {
 		// TODO Auto-generated method stub
-		List<SampleValue> listD1 = percentileValue(listwHeader, 10.0);
+		List<SampleValue> listD1 = percentileValue(sample, 10.0);
 		LOG.debug("résultat D1: {}", listD1);
 		return listD1;
 	}
 
-	public List<SampleValue> calculDecileMax(List<SamplewHeader> listwHeader) {
-		List<SampleValue> listD9 = percentileValue(listwHeader, 90.0);
+	public List<SampleValue> calculDecileMax (SamplewHeader sample) {
+		List<SampleValue> listD9 = percentileValue(sample, 90.0);
 		LOG.debug("résultat D9: {}", listD9);
 		return listD9;
 	}
 	
 //MEDIANE
-	@Override
-	public List<SampleValue> calculMediane(List<SamplewHeader> listwHeader) {
-		List<SampleValue> listmed = percentileValue(listwHeader, 50.0);
+	
+	public List<SampleValue> calculMediane (SamplewHeader sample) {
+		List<SampleValue> listmed = percentileValue(sample, 50.0);
 		LOG.debug("résultat median: {}", listmed);
 		return listmed;
 	}
 
 //QUARTILES
-	@Override
-	public List<SampleValue> calculQ1(List<SamplewHeader> listwHeader) {
-		List<SampleValue> listQ1 = percentileValue(listwHeader, 25.0);
+	public List<SampleValue> calculQ1 (SamplewHeader sample) {
+		List<SampleValue> listQ1 = percentileValue(sample, 25.0);
 		LOG.debug("résultat Q1: {}", listQ1);
 		return listQ1;
 	}
 
-	public List<SampleValue> calculQ3(List<SamplewHeader> listwHeader) {
-		List<SampleValue> listQ3 = percentileValue(listwHeader, 75.0);
+	public List<SampleValue> calculQ3(SamplewHeader sample) {
+		List<SampleValue> listQ3 = percentileValue(sample, 75.0);
 		LOG.debug("résultat Q3: {}", listQ3);
 		return listQ3;
 	}
 	
-	private List<SampleValue> percentileValue(List<SamplewHeader> listwHeader, Double percentile) {
+//	private List<SampleValue> percentileValue(List<SamplewHeader> listwHeader, Double percentile) {
+//		List<SampleValue> listPercentileCal = new ArrayList<>();
+//		for (int i = 0; i < listwHeader.size(); i++) {
+//			Collection<Double> valsampleinter1 = listwHeader.get(i).getSampleGeneVal().values();
+//			Double[] valsampleinter2 = valsampleinter1.toArray(new Double[valsampleinter1.size()]);
+//			double[] valsamplefinal = ArrayUtils.toPrimitive(valsampleinter2);
+//			double percentilecal = StatUtils.percentile(valsamplefinal, percentile);
+//			LOG.debug("decilemin{}", percentilecal);
+//			SampleValue samplevaluespercentile = new SampleValue(listwHeader.get(i).getSampleID(), percentilecal);
+//			listPercentileCal.add(samplevaluespercentile);
+//		}
+//		LOG.debug("resultat percentile{} ", listPercentileCal);
+//		return listPercentileCal;
+//	}
+	
+	private static Logger LOG = LoggerFactory.getLogger(DistributionDesNivExpression.class);
+	
+	
+	//Modification de la methode pour json
+	private List<SampleValue> percentileValue(SamplewHeader sample, Double percentile) {//en argument on ne prend plus la listwheader mais samplweader qui contient deja les valeurs recherchées
 		List<SampleValue> listPercentileCal = new ArrayList<>();
-		for (int i = 0; i < listwHeader.size(); i++) {
-			Collection<Double> valsampleinter1 = listwHeader.get(i).getSampleGeneVal().values();
-			Double[] valsampleinter2 = valsampleinter1.toArray(new Double[valsampleinter1.size()]);
-			double[] valsamplefinal = ArrayUtils.toPrimitive(valsampleinter2);
-			double percentilecal = StatUtils.percentile(valsamplefinal, percentile);
-			LOG.debug("decilemin{}", percentilecal);
-			SampleValue samplevaluespercentile = new SampleValue(listwHeader.get(i).getSampleID(), percentilecal);
+		for (int i = 0; i< sample.getSampleGeneVal().size(); i++){//taille de la map
+			Collection<Double> valsample1= sample.getSampleGeneVal().values();
+			Double[] valsample2 = valsample1.toArray(new Double[valsample1.size()]);
+			double[] valsample3 = ArrayUtils.toPrimitive(valsample2);
+			double percentilecal2 = StatUtils.percentile(valsample3, percentile);
+//          LOG.debug("decilemin{}", percentilecal2);
+			SampleValue samplevaluespercentile = new SampleValue(sample.getSampleID(), percentilecal2);
 			listPercentileCal.add(samplevaluespercentile);
 		}
 		LOG.debug("resultat percentile{} ", listPercentileCal);
 		return listPercentileCal;
 	}
 	
-	private static Logger LOG = LoggerFactory.getLogger(DistributionDesNivExpression.class);
+	
+	
+	
+	
+	
+	
+	
+	
 
 }

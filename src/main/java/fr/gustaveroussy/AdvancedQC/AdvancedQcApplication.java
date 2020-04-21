a42244f967ed86854b80be0816f4f78aa3825e9d
k-hadidja <hadidja.s@gmail.com>
2020-04-21 12:48:09 +0200

    boucle création fichier mqc
    boucle avec ICreationJSON, varibilisation du nom de fichier:ok

--- src/main/java/fr/gustaveroussy/AdvancedQC/AdvancedQcApplication.java
+++ src/main/java/fr/gustaveroussy/AdvancedQC/AdvancedQcApplication.java
@@ -6,14 +6,13 @@ import java.util.List;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files ;
 import java.nio.file.Path;
-
+import fr.gustaveroussy.AdvancedQC.model.SampleValue;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import fr.gustaveroussy.AdvancedQC.model.SampleValue;
 import fr.gustaveroussy.AdvancedQC.model.SamplewHeader;
 import fr.gustaveroussy.AdvancedQC.service.ICreationJSON;
 import fr.gustaveroussy.AdvancedQC.service.IEcritureMqc;
@@ -21,7 +20,6 @@ import fr.gustaveroussy.AdvancedQC.service.IRenvoieDonnesTraitees;
 import fr.gustaveroussy.AdvancedQC.service.impl.CreationBGjson;
 import fr.gustaveroussy.AdvancedQC.service.impl.CreationBWjson;
 import fr.gustaveroussy.AdvancedQC.service.impl.EcritureMqc;
-import fr.gustaveroussy.AdvancedQC.service.impl.RempliMapMoy;
 import fr.gustaveroussy.AdvancedQC.service.impl.RenvoieDonneesTraitees;
 
 @SpringBootApplication
@@ -40,38 +38,27 @@ public class AdvancedQcApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		if (args.length == 2) {
 			IRenvoieDonnesTraitees renvoiMesDonnees = new RenvoieDonneesTraitees();
-			RempliMapMoy mapMoyExpDesGenes = new RempliMapMoy();
             ICreationJSON creationjson1 = new CreationBWjson();
             ICreationJSON creationjson2 =new CreationBGjson();
             ArrayList<ICreationJSON> creationjsonArray= new ArrayList<ICreationJSON>();
             creationjsonArray.add(creationjson1);
             creationjsonArray.add(creationjson2);
-            
- 
+            //String path1 = "/Users/hadidjasaid/data/1";
+            Path path1 = Paths.get(args[1]);
             IEcritureMqc jsonForMqc =new EcritureMqc();
-            
   
 			List<String> lines = Files.readAllLines(Paths.get(args[0]), StandardCharsets.UTF_8);
 			List<SamplewHeader> listwHeader = renvoiMesDonnees.renvoyerDonneesTraitees(lines);
 			LOG.info ("listwheader{}",listwHeader);
-			
-			List<SampleValue> listMeanGeneExpression = mapMoyExpDesGenes.geneExpressionMean(listwHeader);
-			LOG.info("moyenne de taux d'expression des genes {}", listMeanGeneExpression);
 	
 			for (ICreationJSON creationprime : creationjsonArray) {
-				for( int i=1; i< args.length; i++) {
-		            Path path1 = Paths.get(args[i]);
-		            
             	JSONObject filemqc = creationprime.createJSON(listwHeader);
-            	jsonForMqc.ecritureMqc (filemqc ,path1.toString());
+            	jsonForMqc.ecritureMqc (filemqc,path1+ creationprime.getClass().getName().concat("_mqc.json"));
             	LOG.info("filemqc{}",filemqc);
 				}
-            }
 
-			
-		
 		}else {
-			LOG.error("args must at least 2");
+			LOG.error("args must be 2");
 		}
 	}	
 }

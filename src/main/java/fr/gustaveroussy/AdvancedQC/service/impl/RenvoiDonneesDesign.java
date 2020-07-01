822fc16f381e1ea2107b22b719e52b806f0534cf
k-hadidja <hadidja.s@gmail.com>
2020-07-01 14:51:43 +0200

    correction boucle design ok
    modification de la boucle, itération directement sur le lignfichierdesign

--- src/main/java/fr/gustaveroussy/AdvancedQC/service/impl/RenvoiDonneesDesign.java
+++ src/main/java/fr/gustaveroussy/AdvancedQC/service/impl/RenvoiDonneesDesign.java
@@ -2,10 +2,8 @@ package fr.gustaveroussy.AdvancedQC.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import fr.gustaveroussy.AdvancedQC.model.SamplewHeaderwD;
 import fr.gustaveroussy.AdvancedQC.service.IRenvoiDonneesDesign;
 
@@ -20,13 +18,17 @@ public class RenvoiDonneesDesign implements IRenvoiDonneesDesign {
 	lignefichierDesign.remove(0);
 	
 	
-	for (int i = 1; i < colonnewheaderwd.length; i++) {
-	SamplewHeaderwD samplewheaderwd = new SamplewHeaderwD (colonnewheaderwd[2], colonnewheaderwd[3], null);
+	for (String str : lignefichierDesign ){
+		String [] toutesmescolonnes = str.split(spliter);
+	//for (int i = 2; i < colonnewheaderwd.length; i++) {
+	SamplewHeaderwD samplewheaderwd = new SamplewHeaderwD (toutesmescolonnes[2], toutesmescolonnes[3], null);
+	
 	listwheaderwd.add(samplewheaderwd);
 	}
 	LOG.debug("listwheaderwd", listwheaderwd);
 	return listwheaderwd;
 }
+	
 	
 	private static Logger LOG = LoggerFactory
     .getLogger(RenvoiDonneesDesign.class);
